746b95bcee32b2529fc972ea55f60273cebce116
pachecorico <pachecorico001@gmail.com>
2022-11-28 19:25:58 +0100

    creación tabla horarios
    tabla de los horarios

--- fcm.js
+++ fcm.js
@@ -8,37 +8,37 @@ function calcular2(){
     lectura3 = document.getElementById("lectura4");
     lectura4 = document.getElementById("lectura5");
 
-        if(sexo=="hombre"){
-            var frecuenciaress=220-edad;
-            frecuencia.innerHTML=frecuenciaress;
+    if(sexo=="hombre"){
+        var frecuenciaress=220-edad;
+        frecuencia.innerHTML=frecuenciaress;
 
-            var porcentaje=0.6*frecuenciaress;
-            var porcentaje2=0.7*frecuenciaress;
-            var porcentaje3=0.8*frecuenciaress;
-            var porcentaje4=0.9*frecuenciaress;
+        var porcentaje=(0.6*frecuenciaress).toFixed(2);
+        var porcentaje2=(0.7*frecuenciaress).toFixed(2);
+        var porcentaje3=(0.8*frecuenciaress).toFixed(2);
+        var porcentaje4=(0.9*frecuenciaress).toFixed(2);
 
-            
-            lectura.innerHTML = porcentaje+" Zona recuperación"; 
-            lectura2.innerHTML = porcentaje2+" Zona aeróbica"; 
-            lectura3.innerHTML = porcentaje3+" Zona anaeróbica"; 
-            lectura4.innerHTML = porcentaje4+" Línea roja"; 
-        }
-        else if(sexo=="mujer"){
-            var frecuenciares=226-edad;
-            frecuencia.innerHTML=frecuenciares;
-        
-            var porcentaje=0.6*frecuenciares;
-            var porcentaje2=0.7*frecuenciares;
-            var porcentaje3=0.8*frecuenciares;
-            var porcentaje4=0.9*frecuenciares;
 
-            
-            lectura.innerHTML = porcentaje+" Zona recuperación"; 
-            lectura2.innerHTML = porcentaje2+" Zona aeróbica"; 
-            lectura3.innerHTML = porcentaje3+" Zona anaeróbica"; 
-            lectura4.innerHTML = porcentaje4+" Línea roja"; 
-        }else{
-            alert("Debes ingresar edad y sexo.")
-        }
-    
+        lectura.innerHTML = porcentaje+"-"+porcentaje2+" Zona recuperación"; 
+        lectura2.innerHTML = porcentaje2+"-"+porcentaje3+" Zona aeróbica"; 
+        lectura3.innerHTML = porcentaje3+"-"+porcentaje4+" Zona anaeróbica"; 
+        lectura4.innerHTML = porcentaje4+" Línea roja";
+    }
+    else if(sexo=="mujer"){
+        var frecuenciares=226-edad;
+        frecuencia.innerHTML=frecuenciares;
+
+        var porcentaje=(0.6*frecuenciares).toFixed(2);
+        var porcentaje2=(0.7*frecuenciares).toFixed(2);
+        var porcentaje3=(0.8*frecuenciares).toFixed(2);
+        var porcentaje4=(0.9*frecuenciares).toFixed(2);
+
+
+        lectura.innerHTML = porcentaje+"-"+porcentaje2+" Zona recuperación"; 
+        lectura2.innerHTML = porcentaje2+"-"+porcentaje3+" Zona aeróbica"; 
+        lectura3.innerHTML = porcentaje3+"-"+porcentaje4+" Zona anaeróbica"; 
+        lectura4.innerHTML = porcentaje4+" Línea roja";
+    }else{
+        alert("Debes ingresar edad y sexo.")
+    }
+
 };
--- horarios.js
+++ horarios.js
@@ -1,65 +1,22 @@
-//Crea una tabla de horario de mañana que muestre las horas de dos en dos con el formato 9:00-11:00, desde las 9 hasta las 15:00 y los dı́as del lunes al viernes.Habrá otra tabla de horario de tarde que muestre las horas de una en una con el formato 16:00-17:00, desde las 16 hasta las 21:00 y los dı́as del lunes al domingo.Tanto las horas como los dı́as (es decir, la primera columna y la primera Cila) tendrán etiqueta th. El resto tendrán etiqueta td.
-
 function horarioManana() {
-    var tabla = document.createElement("table");
-    var cabecera = document.createElement("tr");
-    var celda = document.createElement("th");
-    var texto = document.createTextNode("Horario");
-    celda.appendChild(texto);
-    cabecera.appendChild(celda);
-    var dias = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes"];
-    for (var i = 0; i < dias.length; i++) {
-        var celda = document.createElement("th");
-        var texto = document.createTextNode(dias[i]);
-        celda.appendChild(texto);
-        cabecera.appendChild(celda);
-    }
-    tabla.appendChild(cabecera);
-    for (var i = 9; i < 15; i += 2) {
-        var fila = document.createElement("tr");
-        var celda = document.createElement("th");
-        var texto = document.createTextNode(i + ":00-" + (i + 2) + ":00");
-        celda.appendChild(texto);
-        fila.appendChild(celda);
-        for (var j = 0; j < dias.length; j++) {
-            var celda = document.createElement("td");
-            var texto = document.createTextNode(" ");
-            celda.appendChild(texto);
-            fila.appendChild(celda);
+    lectura = document.getElementById("tabla");
+    let hola=" ";
+    hola = "<table border='1'>";
+    hola += "<tr><th>HORA</th><th>LUNES</th><th>MARTES</th><th>MIERCOLES</th><th>JUEVES</th><th>VIERNES</th></tr>";
+    for (let i = 9; i < 15; i += 2) {
+        hola += "<tr>";
+        hola += "<th>" + i + ":00 - " + (i + 2) + ":00</th>";
+        for (let j = 0; j < 5; j++) {
+            hola += "<td></td>";
         }
-        tabla.appendChild(fila);
+        hola += "</tr>";
     }
-    document.body.appendChild(tabla);
+    hola += "</table>";
+    lectura.innerHTML = hola;
 }
 
-function horarioTarde() {
-    var tabla = document.createElement("table");
-    var cabecera = document.createElement("tr");
-    var celda = document.createElement("th");
-    var texto = document.createTextNode("Horario");
-    celda.appendChild(texto);
-    cabecera.appendChild(celda);
-    var dias = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"];
-    for (var i = 0; i < dias.length; i++) {
-        var celda = document.createElement("th");
-        var texto = document.createTextNode(dias[i]);
-        celda.appendChild(texto);
-        cabecera.appendChild(celda);
-    }
-    tabla.appendChild(cabecera);
-    for (var i = 16; i < 22; i++) {
-        var fila = document.createElement("tr");
-        var celda = document.createElement("th");
-        var texto = document.createTextNode(i + ":00-" + (i + 1) + ":00");
-        celda.appendChild(texto);
-        fila.appendChild(celda);
-        for (var j = 0; j < dias.length; j++) {
-            var celda = document.createElement("td");
-            var texto = document.createTextNode(" ");
-            celda.appendChild(texto);
-            fila.appendChild(celda);
-        }
-        tabla.appendChild(fila);
-    }
-    document.body.appendChild(tabla);
-}
+
+    
+
+
+
