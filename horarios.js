ac0d79b1d4191c6b31ab5df91202ef95345388a1
pachecorico <pachecorico001@gmail.com>
2022-11-29 18:14:20 +0100

    creación aviso legal y formulario de contacto
    creación aviso legal y formulario de contact

--- horarios.js
+++ horarios.js
@@ -14,6 +14,24 @@ function horarioManana() {
     hola += "</table>";
     lectura.innerHTML = hola;
 }
+ function horarioTarde() {
+    lectura = document.getElementById("tabla");
+    let hola=" ";
+    hola = "<table border='1'>";
+    hola += "<tr><th>HORA</th><th>LUNES</th><th>MARTES</th><th>MIERCOLES</th><th>JUEVES</th><th>VIERNES</th><th>SABADO</th><th>DOMINGO</th></tr>";
+    for (let i = 15; i < 21; i += 2) {
+        hola += "<tr>";
+        hola += "<th>" + i + ":00 - " + (i + 2) + ":00</th>";
+        for (let j = 0; j < 7; j++) {
+            hola += "<td></td>";
+        }
+        hola += "</tr>";
+    }
+    hola += "</table>";
+    lectura.innerHTML = hola;
+}
+
+ 
 
 
     
